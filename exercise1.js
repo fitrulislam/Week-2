a2fe54657276b87bcf6eebdac20b6f0988d3288c
Fitrul Islam <fitrul.islam@gmail.com>
2017-12-19 23:42:54 +0700

    fresh

---
+++ exercise1.js
@@ -0,0 +1,12 @@
+var number1 = 33;
+var number2 = 6;
+var string1 = "foo";
+var string2 = "bar";
+var boolean1 = 5;
+
+console.log(number1);
+console.log(number2);
+console.log(string1);
+console.log(string2);
+console.log(boolean1 == 5);
+console.log(boolean1 == 6);
---
+++ exercise2.js
@@ -0,0 +1,38 @@
+var nama = 'Gatotkaca';
+var peran = '';
+
+if (nama == 'Gatotkaca') {
+  console.log('Halo '+ nama + ', pilih peranmu untuk memulai game!');
+} else {
+  console.log('Ganti game aja');
+}
+
+var nama = 'Miya';
+var peran = 'Marksman';
+
+if (nama == 'Miya') {
+  console.log('Selamat datang di dunia Proxytia, '+ nama);
+  console.log('Halo '+ peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+} else {
+  console.log('Ganti game aja');
+}
+
+var nama = 'Rafaela';
+var peran = 'Healer';
+
+if (nama == 'Rafaela') {
+  console.log('Selamat datang di dunia Proxytia, '+ nama);
+  console.log('Halo '+ peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka.');
+} else {
+  console.log ('Ganti game aja');
+}
+
+var nama = 'Aurora';
+var peran = 'Mage';
+
+if (nama == 'Aurora') {
+  console.log('Selamat datang di dunia Proxytia, '+ nama);
+  console.log('Halo'+ peran + ' ' + nama + ', ciptakan keajaiban yang akan membantu kemenanganmu!');
+} else {
+  console.log ('Ganti game aja');
+}
