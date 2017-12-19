20fa064bc5587ad5e7975731a3466f038dd01596
Fitrul Islam <fitrul.islam@gmail.com>
2017-12-20 00:03:02 +0700

    fresh

---
+++ exercise4.js
@@ -0,0 +1,13 @@
+var number = 2;
+while(number <= 20) {
+  console.log(number + ' - I love coding');
+  number++;
+  number++;
+}
+
+var number = 20;
+while(number >= 2) {
+  console.log(number + ' - I will become full stack developer');
+  number--;
+  number--;
+}
---
+++ exercise5.js
@@ -0,0 +1,7 @@
+for(var number = 1; number <= 20; number++) {
+  console.log(number + ' - I love coding');
+}
+
+for(var number = 20; number >= 1; number--) {
+  console.log(number + ' - I will become full stack developer');
+}
