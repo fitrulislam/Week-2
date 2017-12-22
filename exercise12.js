3361be36f09cc8ff6dbe38f03cb5353b7b4b201f
Fitrul Islam <fitrul.islam@gmail.com>
2017-12-22 10:43:21 +0700

    yes!

---
+++ exercise12.js
@@ -0,0 +1,7 @@
+var balikString = 'Hello World!';
+var tampung = '';
+  for (var num = 1; num <=12; num++) {
+  balik = balikString[balikString.length - num];
+  tampung = tampung + balik;
+  }
+  console.log(tampung);
