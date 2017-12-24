67f82cf83dda3f51c03fa527da9de10531502516
Fitrul Islam <fitrul.islam@gmail.com>
2017-12-24 18:43:17 +0700

    fresh

---
+++ pseudocode-todolist.md
@@ -0,0 +1,31 @@
+TAMPILKAN judul kegiatan
+
+Buat FUNCTION bernama 'toDoList' dengan parameter 'kegiatan'
+
+Di dalam fungsi 'toDoList', buat looping dengan FOR, FOR digunakan untuk menampilkan kegiatan sesuai waktu. Nama variabel adalah 'jam', dengan batasan angka antara 4 sampai 22 (waktu aktivitas manusia selain tidur) dan looping dengan pertambahan +1
+
+Di dalam looping FOR, dibuat kondisional if-else yang menampilkan kegiatan setiap pertambahan satu angka
+
+Kondisi pertama 'jam' menunjukkan angka 4, buat console.log berisi bangun tidur dan olahraga
+
+Kondisi kedua 'jam' menunjukkan angka 5 atau 12 atau 15 atau 16 atau 19, buat console.log berisi ibadah
+
+Kondisi ketiga 'jam' menunjukkan angka 7 atau 13 atau 20, buat console.log berisi makan
+
+Kondisi keempat 'jam' menunjukkan angka 6 atau 17, buat console.log berisi mandi
+
+Kondisi kelima 'jam' menunjukkan angka 8 atau 9 atau 14, buat console.log berisi coding
+
+Kondisi keenam 'jam' menunjukkan angka 10 atau 16, buat console.log berisi bantu bantu di rumah
+
+Kondisi ketujuh 'jam' menunjukkan angka 11, buat console.log berisi main game
+
+Kondisi kedelapan 'jam' menunjukkan angka 21, buat console.log berisi nonton tv atau ngoding
+
+Diluar delapan kondisi yang ada, ELSE menunjukkan saatnya tidur
+
+DEKLARASI array berisi kegiatan yang akan dilakukan yaitu: tidur, ibadah, makan, mandi, ngoding, bantu-bantu di rumah, nonton TV, olahraga, ngegame
+
+TAMPILKAN hasil, console.log dari FUNCTION dengan parameternya
+
+beri pesan pesan
---
+++ todolist.js
@@ -0,0 +1,46 @@
+//Judul kegiatan
+console.log('To Do List di Hari Minggu! (24 Desember 2017)');
+console.log('');
+
+//fungsi untuk menampilkan to do list
+function toDoList(kegiatan) {
+  for(var jam = 4; jam <= 22; jam++){
+    if(jam===4){
+      console.log('Jam ' + jam + ':00 WIB: Bangun ' + kegiatan[0] + ', ' + kegiatan[7] + ' bentar');
+      console.log();
+    } else if(jam===5 ||jam===12||jam===15||jam===18||jam===19){
+      console.log('Jam ' + jam + ':00 WIB: Saatnya ' + kegiatan[1] + '!');
+      console.log();
+    } else if(jam===7||jam===13||jam===20) {
+      console.log('Jam ' + jam + ':00 WIB: Isi tenaga coy, ' + kegiatan[2] + ' dulu~');
+      console.log();
+    } else if(jam===6||jam===17) {
+      console.log('Jam ' + jam + ':00 WIB: ' + kegiatan[3] + ' dulu pak, biar ga bau.');
+      console.log();
+    } else if(jam===8||jam===9||jam===14) {
+      console.log('Jam ' + jam + ':00 WIB: Latihan ' + kegiatan[4] + ', biar pinter.');
+      console.log();
+    } else if(jam===10||jam===16) {
+      console.log('Jam ' + jam + ':00 WIB: ' + kegiatan[5]);
+      console.log();
+    } else if(jam===11){
+      console.log('Jam ' + jam + ':00 WIB: ' + kegiatan[8] + ' biar ga stress');
+      console.log();
+    } else if(jam===21){
+      console.log('Jam ' + jam + ':00 WIB: ' + kegiatan[6] + ' atau ' + kegiatan[4] + ' lagi~');
+      console.log();
+    } else {
+      console.log('Jam ' + jam + ':00 WIB: Saatnya ' + kegiatan[0]);
+      console.log();
+    }
+  }
+  return;
+}
+
+//deklarasi variabel
+var kegiatan = ['tidur', 'ibadah', 'makan', 'Mandi', 'ngoding', 'Bantu-bantu di rumah', 'Nonton TV', 'olahraga', 'Ngegame'];
+
+//menampilkan hasil
+console.log(toDoList(kegiatan));
+
+console.log('nb: Jangan lupa main game kalo ada waktu kosong tiap selese satu kegiatan :v');
