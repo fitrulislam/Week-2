c99f76ff1255c0a202072386cf1d95decc834780
Fitrul Islam <fitrul.islam@gmail.com>
2018-01-03 12:09:33 +0700

    lebih sederhana

--- exercise12.js
+++ exercise12.js
@@ -1,7 +1,6 @@
 var balikString = 'Hello World!';
 var tampung = '';
-  for (var num = 1; num <=balikString.length; num++) {
-  balik = balikString[balikString.length - num];
-  tampung = tampung + balik;
+  for (var num = balikString.length-1; num>=0; num--) {
+  tampung = tampung + balikString[num];
   }
   console.log(tampung);
